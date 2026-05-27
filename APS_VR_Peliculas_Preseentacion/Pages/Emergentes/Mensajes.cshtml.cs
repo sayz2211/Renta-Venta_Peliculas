a1ebdc931a7b3c5d3f4f0afc26f625f311199008
Samuel Yepes <samuelyepes1122020@correo.itm.edu.co>
2026-05-27 18:31:43 -0500

    cambios menores

---
+++ APS_VR_Peliculas_Preseentacion/Pages/Emergentes/Mensajes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace APS_VR_Peliculas_Presentacion.Pages.Emergentes
+{
+    public class MensajeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- APS_VR_Peliculas_Preseentacion/obj/Debug/net8.0/APS_VR_Peliculas_Presentacion.AssemblyInfo.cs
+++ APS_VR_Peliculas_Preseentacion/obj/Debug/net8.0/APS_VR_Peliculas_Presentacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APS_VR_Peliculas_Presentacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8bb5c6381e468b77bd444512decc208fabbf7a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96c0e18e0b4bd7de293000a5c883989bd8df4634")]
 [assembly: System.Reflection.AssemblyProductAttribute("APS_VR_Peliculas_Presentacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APS_VR_Peliculas_Presentacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
